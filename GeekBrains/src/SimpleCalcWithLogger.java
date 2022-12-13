83735ca1ba18c85cc1b4a53b539c1b35948b91e0
Vladimir Mikhailov <vladimir@mikhailov.in>
2022-12-13 23:21:41 +0300

    Seminar2 Homework

---
+++ GeekBrains/src/SimpleCalcWithLogger.java
@@ -0,0 +1,52 @@
+// Семинар 2.
+// Задание 4. К калькулятору из предыдущего дз добавить логирование.
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class SimpleCalcWithLogger {
+    public static void main(String[] args) throws IOException {
+        Logger logger = BubbleLog.getLogger("GeekBrains/src/SimpleCalcWithLogger.txt");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Введите число А: ");
+        float a = input.nextFloat();
+
+        System.out.print("Введите число Б: ");
+        float b = input.nextFloat();
+
+        System.out.print("Введите операцию '/', '*', '+' или '-': ");
+        char op = input.next().charAt(0);
+
+        StringBuilder msg = new StringBuilder();
+        float res = calc(a, b, op);
+        msg
+                .append(a).append(" ")
+                .append(op).append(" ")
+                .append(b).append(" ")
+                .append("=").append(" ")
+                .append(String.format("%.2f", res));
+
+        logger.info(msg.toString());
+        System.out.printf("Результат: %.2f\n", res);
+    }
+
+    public static float calc(float a, float b, char operation) {
+        switch (operation) {
+            case '/' -> {
+                return a / b;
+            }
+            case '*' -> {
+                return a * b;
+            }
+            case '+' -> {
+                return a + b;
+            }
+            case '-' -> {
+                return a - b;
+            }
+        }
+        return 0;
+    }
+}
