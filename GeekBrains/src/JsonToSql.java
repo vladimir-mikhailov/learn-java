f9797d107d1f5a30bd3834f2756fcb35da1d045a
Vladimir <75040504+vladimir-mikhailov@users.noreply.github.com>
2022-12-15 00:33:51 +0300

    Seminar2 Homework (#2)
    * Seminar2 Homework

---
+++ GeekBrains/src/JsonToSql.java
@@ -0,0 +1,55 @@
+// Семинар 2.
+// Задание 1. Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса,
+// используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки.
+// Если значение null, то параметр не должен попадать в запрос.
+// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+
+public class JsonToSql {
+    public static void main(String[] args) throws IOException {
+        String fileName = "GeekBrains/src/JsonToSql.json";
+        System.out.println(getSQL(fileName));
+    }
+
+    private static String getSQL(String fileName) throws IOException {
+        FileReader f = null;
+        try {
+            f = new FileReader(fileName);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Нет такого файла.");
+        }
+
+        StringBuilder sql = new StringBuilder();
+        if (f != null) {
+            Gson gson = new Gson();
+            TypeToken<Map<String, String>> mapType = new TypeToken<>() {
+            };
+            Map<String, String> studentMap = gson.fromJson(f, mapType);
+
+            sql.append("SELECT * FROM students WHERE ");
+            int counter = 0;
+            for (Map.Entry<String, String> student : studentMap.entrySet()) {
+                if (!Objects.equals(student.getValue(), "null")) {
+                    if (counter > 0) {
+                        sql.append(" AND ");
+                    }
+                    sql.append(student.getKey()).append(" = '").append(student.getValue()).append("'");
+                    counter++;
+                }
+            }
+            sql.append(';');
+            f.close();
+        }
+        return sql.toString();
+    }
+}
---
+++ GeekBrains/src/JsonToString.java
@@ -0,0 +1,57 @@
+// Семинар 2.
+// Задание 3. Дана json строка (можно сохранить в файл и читать из файла)
+// [
+//  {"фамилия":"Иванов","оценка":"5","предмет":"Математика"},
+//  {"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},
+//  {"фамилия":"Краснов","оценка":"5","предмет":"Физика"}
+// ]
+// Написать метод(ы), который распарсит json и, используя StringBuilder, создаст строки вида:
+// Студент [фамилия] получил [оценка] по предмету [предмет].
+// Пример вывода:
+// Студент Иванов получил 5 по предмету Математика.
+// Студент Петрова получил 4 по предмету Информатика.
+// Студент Краснов получил 5 по предмету Физика.
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class JsonToString {
+    public static void main(String[] args) throws IOException {
+        String jsonFile = "GeekBrains/src/JsonToString.json";
+        System.out.println(getReport(jsonFile));
+    }
+
+    private static String getReport(String fileName) throws IOException {
+        FileReader f = null;
+        try {
+            f = new FileReader(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("Нет такого файла.");
+        }
+
+        StringBuilder reportText = new StringBuilder();
+        if (f != null) {
+            Gson gson = new Gson();
+            TypeToken<List<Map<String, String>>> mapType = new TypeToken<>() {
+            };
+            List<Map<String, String>> marksList = gson.fromJson(f, mapType);
+
+            for (Map<String, String> mark : marksList) {
+                reportText
+                        .append("Студент ").append(mark.get("фамилия"))
+                        .append(" получил ").append(mark.get("оценка"))
+                        .append(" по предмету ").append(mark.get("предмет"))
+                        .append(".\n");
+            }
+
+            f.close();
+        }
+        return reportText.toString();
+    }
+}
