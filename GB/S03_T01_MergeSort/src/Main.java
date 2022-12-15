bf4648ee62009c1e59d8ff0299131b59adce7732
Vladimir Mikhailov <vladimir@mikhailov.in>
2022-12-16 01:15:53 +0300

    upd: s3 task3 ArrayList -> LinkedList (faster?)

--- GB/S03_T01_MergeSort/src/Main.java
+++ GB/S03_T01_MergeSort/src/Main.java
@@ -1,6 +1,6 @@
 //1. Реализовать алгоритм сортировки слиянием
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,46 +10,46 @@ public class Main {
         // Если установить "1", то будет только сортировка слиянием.
         int chunkSize = 1;
 
-        ArrayList<Integer> list = new ArrayList<>(listSize);
+        LinkedList<Integer> list = new LinkedList<>();
         for (int i = 0; i < listSize; i++) {
             list.add((int) (Math.random() * 100));
         }
         System.out.println(list);
 
         for (int ch = chunkSize; ch < listSize; ch *= 2) {
-                ArrayList<Integer> buffer = new ArrayList<>(listSize);
-                for (int l = 0; l < listSize; l += ch * 2) {
-                    ArrayList<Integer> left, right;
+            LinkedList<Integer> buffer = new LinkedList<>();
+            for (int l = 0; l < listSize; l += ch * 2) {
+                LinkedList<Integer> left, right;
 
-                    if (ch >= listSize - 1 - l) {
-                        left = new ArrayList<>(list.subList(l, listSize));
-                        if (ch == chunkSize && left.size() > 1) {
-                            left.sort(null);
-                        }
-                        buffer.addAll(left);
-                    } else {
-                        left = new ArrayList<>(list.subList(l, l + ch));
-                        right = new ArrayList<>(list.subList(l + ch, Math.min(l + ch * 2, listSize)));
-                        if (ch == chunkSize) {
-                            if (left.size() > 1) left.sort(null);
-                            if (right.size() > 1) right.sort(null);
-                        }
-                        buffer.addAll(mergeSort(left, right));
+                if (ch >= listSize - 1 - l) {
+                    left = new LinkedList<>(list.subList(l, listSize));
+                    if (ch == chunkSize && left.size() > 1) {
+                        left.sort(null);
                     }
+                    buffer.addAll(left);
+                } else {
+                    left = new LinkedList<>(list.subList(l, l + ch));
+                    right = new LinkedList<>(list.subList(l + ch, Math.min(l + ch * 2, listSize)));
+                    if (ch == chunkSize) {
+                        if (left.size() > 1) left.sort(null);
+                        if (right.size() > 1) right.sort(null);
+                    }
+                    buffer.addAll(mergeSort(left, right));
                 }
-                list = new ArrayList<>(buffer);
+            }
+            list = new LinkedList<>(buffer);
         }
         System.out.println(list);
 
     }
 
-    public static ArrayList<Integer> mergeSort(ArrayList<Integer> left, ArrayList<Integer> right) {
-        ArrayList<Integer> res = new ArrayList<>(left.size() + right.size());
+    public static LinkedList<Integer> mergeSort(LinkedList<Integer> left, LinkedList<Integer> right) {
+        LinkedList<Integer> res = new LinkedList<>();
         int i = 0, j = 0;
         while (i < left.size() && j < right.size()) {
             res.add(left.get(i) <= right.get(j) ? left.get(i++) : right.get(j++));
-            if (i == left.size()) res.addAll(new ArrayList<>(right.subList(j, right.size())));
-            if (j == right.size()) res.addAll(new ArrayList<>(left.subList(i, left.size())));
+            if (i == left.size()) res.addAll(new LinkedList<>(right.subList(j, right.size())));
+            if (j == right.size()) res.addAll(new LinkedList<>(left.subList(i, left.size())));
         }
         return res;
     }
