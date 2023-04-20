97f17d4b6080a9e61580f6c0920b9438a8ae8b21
Vladimir <vladimir@mikhailov.in>
2023-04-20 19:53:53 +0300

    Algo Seminar 4: Left Leaning Red Black Tree

---
+++ GB/RedBlackTree/src/Main.java
@@ -0,0 +1,14 @@
+import Tree.RedBlackTree;
+
+public class Main {
+    public static void main(String[] args) {
+        RedBlackTree tree = new RedBlackTree();
+        tree.add(1);
+        tree.add(11);
+        tree.add(111);
+        tree.add(2);
+        tree.add(22);
+        tree.add(222);
+    }
+}
+
